d01f0e8cfe4adf2a212ee3aafcb85de4072bab40
dev <dev@musen.co.jp>
2024-10-18 17:48:19 +0900

    メタデータを定義するための構造体を作成

--- src/writer.rs
+++ src/writer.rs
@@ -1,16 +1,43 @@
 use std::io::{Seek, Write};
 
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Utc};
-use mp4::{BoxHeader, BoxType, FourCC, FtypBox, MoovBox, WriteBox};
+use mp4::{
+    Avc1Box, BoxHeader, BoxType, FourCC, FtypBox, Hev1Box, MoovBox, Mp4aBox, MvexBox, StblBox,
+    TrakBox, Tx3gBox, Vp09Box, WriteBox,
+};
 
-use crate::Result;
+use crate::{error::Fmp4ParseError, segment::InitialSegment, Result};
 
-#[derive(Debug, Default)]
-pub struct FMp4Config {
-    pub major_brand: FourCC,
-    pub minor_version: u32,
-    pub compatible_brands: Vec<FourCC>,
-    pub timescale: u32,
+#[derive(Debug)]
+pub enum MediaBox {
+    Avc1(Avc1Box),
+    Hev1(Hev1Box),
+    Vp9(Vp09Box),
+    Mp4a(Mp4aBox),
+    Tx3g(Tx3gBox),
+    __Unknown,
+}
+
+impl From<&StblBox> for MediaBox {
+    fn from(value: &StblBox) -> Self {
+        if let Some(ref avc1) = value.stsd.avc1 {
+            MediaBox::Avc1(avc1.clone())
+        } else if let Some(ref hev1) = value.stsd.hev1 {
+            MediaBox::Hev1(hev1.clone())
+        } else if let Some(ref vp09) = value.stsd.vp09 {
+            MediaBox::Vp9(vp09.clone())
+        } else if let Some(ref mp4a) = value.stsd.mp4a {
+            MediaBox::Mp4a(mp4a.clone())
+        } else if let Some(ref tx3g) = value.stsd.tx3g {
+            MediaBox::Tx3g(tx3g.clone())
+        } else {
+            MediaBox::__Unknown
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TrackExtendData {
     pub track_id: u32,
     pub default_sample_description_index: u32,
     pub default_sample_duration: u32,
@@ -18,11 +45,77 @@ pub struct FMp4Config {
     pub default_sample_flags: u32,
 }
 
+impl From<&MvexBox> for TrackExtendData {
+    fn from(value: &MvexBox) -> Self {
+        Self {
+            track_id: value.trex.track_id,
+            default_sample_description_index: value.trex.default_sample_description_index,
+            default_sample_duration: value.trex.default_sample_duration,
+            default_sample_size: value.trex.default_sample_size,
+            default_sample_flags: value.trex.default_sample_flags,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TrackData {
+    pub width: u16,
+    pub height: u16,
+    pub timescale: u32,
+    pub media_box: MediaBox,
+}
+
+impl From<&TrakBox> for TrackData {
+    fn from(value: &TrakBox) -> Self {
+        Self {
+            width: value.tkhd.width.value(),
+            height: value.tkhd.height.value(),
+            timescale: value.mdia.mdhd.timescale,
+            media_box: MediaBox::from(&value.mdia.minf.stbl),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FMp4Config {
+    major_brand: FourCC,
+    minor_version: u32,
+    compatible_brands: Vec<FourCC>,
+    tracks: Vec<(TrackData, TrackExtendData)>,
+}
+
+impl FMp4Config {
+    pub fn add_track(&mut self, initial_segment: &InitialSegment) -> Result<()> {
+        let extend_data = TrackExtendData::from(
+            initial_segment
+                .moov
+                .mvex
+                .as_ref()
+                .ok_or(Fmp4ParseError::InvalidFormat("Missing mvex box"))?,
+        );
+
+        let Some(trak) = initial_segment
+            .moov
+            .traks
+            .iter()
+            .find(|trak| trak.tkhd.track_id == extend_data.track_id)
+        else {
+            return Err(Fmp4ParseError::InvalidFormat("Missing trak box"));
+        };
+
+        let track_data = TrackData::from(trak);
+        self.tracks.push((track_data, extend_data));
+
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 pub struct HybridMp4Writer<W> {
     writer: W,
     free_pos: u64,
     moov: MoovBox,
+    // tracks: HashMap<u32, TrackExtends>,
 }
 
 impl<W: Write + Seek> HybridMp4Writer<W> {
@@ -44,6 +137,7 @@ impl<W: Write + Seek> HybridMp4Writer<W> {
             writer,
             free_pos,
             moov,
+            // tracks: HashMap::new(),
         })
     }
 
@@ -60,6 +154,10 @@ impl<W: Write + Seek> HybridMp4Writer<W> {
         self.moov.mvhd.creation_time = creation_time;
         self.moov.mvhd.modification_time = creation_time;
 
+        // while let Some(track_extend) = self.tracks.get(&self.moov.mvhd.next_track_id) {
+        //     self.moov.mvhd.next_track_id += 1;
+        // }
+
         println!("{:?}", self.moov);
 
         Ok(())
@@ -72,8 +170,8 @@ mod tests {
 
     #[test]
     fn test_writer_hybrid_mp4() {
-        let file = std::fs::File::create("resources/test.mp4").unwrap();
-        let mut writer = HybridMp4Writer::initialize(file, &FMp4Config::default()).unwrap();
-        writer.finalize().unwrap();
+        // let file = std::fs::File::create("resources/test.mp4").unwrap();
+        // let mut writer = HybridMp4Writer::initialize(file, &FMp4Config::default()).unwrap();
+        // writer.finalize().unwrap();
     }
 }
